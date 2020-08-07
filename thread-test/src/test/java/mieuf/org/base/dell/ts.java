cc0241f634f09f541a86eb3271d34383b3ff4639
mieuf <1148087814@qq.com>
2020-08-07 17:11:08 +0800

    Create ts.java

---
+++ thread-test/src/test/java/mieuf/org/base/dell/ts.java
@@ -0,0 +1,38 @@
+package org.base.mvc.thr;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * @Date 2020/8/7 15:16
+ * @Created by zhangbing
+ * @Description TODO
+ */
+
+public class ts {
+    private static int apple = 10;
+    private int orange = 10;
+
+    public static void main(String[] args) throws Exception {
+        Unsafe unsafe = getUnsafeInstance();
+
+        Field appleField = ts.class.getDeclaredField("apple");
+        System.out.println("Location of Apple: " + unsafe.staticFieldOffset(appleField));
+
+        Field orangeField = ts.class.getDeclaredField("orange");
+        System.out.println("Location of Orange: " + unsafe.objectFieldOffset(orangeField));
+
+
+        System.out.println(orangeField.getInt(new ts()));
+        System.out.println(appleField.getInt(new ts()));
+    }
+
+    private static Unsafe getUnsafeInstance() throws SecurityException, NoSuchFieldException, IllegalArgumentException,
+            IllegalAccessException {
+        Field theUnsafeInstance = Unsafe.class.getDeclaredField("theUnsafe");
+        theUnsafeInstance.setAccessible(true);
+        return (Unsafe) theUnsafeInstance.get(Unsafe.class);
+    }
+
+}
